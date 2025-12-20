7433446ab8526be4985bf9dd50f25e0cf5c49f8c
Pascual Madrid <pascualm4@gmail.com>
2025-12-20 14:26:50 -0300

    chore: create basic layout

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,50 +1,34 @@
 import { Box, Grid } from "@mui/material";
-import Sidebar from "../../components/dashboard/Sidebar";
-import Header from "../../components/dashboard/Header";
-import InvoiceBanner from "../../components/dashboard/InvoiceBanner";   
-import RetainedInvoicesCard from "../../components/dashboard/RetainedInvoicesCard"; 
-import DueInvoicesCard from "../../components/dashboard/DueInvoicesCard";
-import PendingInvoicesTabs from "../../components/dashboard/PendingInvoicesTabs";
+import Layout from "../../components/Layout";
+import InvoiceBanner from "../../components/Dashboard/InvoiceBanner";
+import RetainedInvoicesCard from "../../components/Dashboard/RetainedInvoicesCard";
+import DueInvoicesCard from "../../components/Dashboard/DueInvoicesCard";
+import PendingInvoicesTabs from "../../components/Dashboard/PendingInvoicesTabs";
 
 const Dashboard = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        minHeight: "100vh",
-        backgroundColor: "background.default",
-      }}
-    >
-      {/* Sidebar */}
-      <Sidebar />
+    <Layout>
+      {/* Content Area */}
+      <Box sx={{ p: 3, flex: 1 }}>
+        {/* Invoice Banner */}
+        <InvoiceBanner />
 
-      {/* Main Content */}
-      <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
-        {/* Header */}
-        <Header />
-
-        {/* Content Area */}
-        <Box sx={{ p: 3, flex: 1 }}>
-          {/* Invoice Banner */}
-          <InvoiceBanner />
-
-          {/* Cards Row */}
-          <Grid container spacing={3} sx={{ mt: 2 }}>
-            <Grid size={{ xs: 12, lg: 5 }}>
-              <RetainedInvoicesCard />
-            </Grid>
-            <Grid size={{ xs: 12, lg: 7 }}>
-              <DueInvoicesCard />
-            </Grid>
+        {/* Cards Row */}
+        <Grid container spacing={3} sx={{ mt: 2 }}>
+          <Grid size={{ xs: 12, lg: 5 }}>
+            <RetainedInvoicesCard />
           </Grid>
+          <Grid size={{ xs: 12, lg: 7 }}>
+            <DueInvoicesCard />
+          </Grid>
+        </Grid>
 
-          {/* Pending Invoices Tabs */}
-          <Box sx={{ mt: 3 }}>
-            <PendingInvoicesTabs />
-          </Box>
+        {/* Pending Invoices Tabs */}
+        <Box sx={{ mt: 3 }}>
+          <PendingInvoicesTabs />
         </Box>
       </Box>
-    </Box>
+    </Layout>
   );
 };
 
