35ff7d1c7765d6d24427c4c2b9ad4aa803b4ebab
Pascual Madrid <pascualm4@gmail.com>
2026-01-03 15:40:59 -0300

    use the current context to show the pill in the top of the header

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,8 @@ import { ROLES } from "./consts";
 export const getRoleNameByCode = (roleCode: string) => {
   return (ROLES as Record<string, string>)[roleCode] || ROLES.DEFAULT;
 }
+
+export const capitalizeString = (str: string): string => {
+  if (!str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
