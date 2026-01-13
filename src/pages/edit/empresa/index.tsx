e1d259233e7de74886d81845370b7c3f80caba4a
Pascual Madrid <pascualm4@gmail.com>
2026-01-13 19:02:06 -0300

    include sii form

--- src/pages/edit/empresa/index.tsx
+++ src/pages/edit/empresa/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import AccountLevels from "../../../components/Edit/AccountLevels";
+import SiiSync from "../../../components/Edit/SiiSync";
 import Empresa from "../../../components/Edit/Empresa";
 import useAuthStore from "../../../store/authStore";
 
@@ -9,6 +10,7 @@ const EmpresaTab = () => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       <AccountLevels currentLevel={currentRole?.nivel as 1 | 2 | 3 | undefined} />
+      <SiiSync />
       <Empresa />
     </Box>
   );
